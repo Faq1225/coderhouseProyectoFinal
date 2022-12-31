cdfcacfdb2a730622a0c267922963fe1daee0eda
Facundo Mercado <facu.mercado1224@gmail.com>
2022-12-31 17:44:13 -0300

    Segundo commit. Pequeños ajustes

--- proyectoFinal/Usuarios/urls.py
+++ proyectoFinal/Usuarios/urls.py
@@ -1,5 +1,6 @@
 from django.urls import *
 from Usuarios.views import *
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 
 UUID_CANAL_REGEX = r'canal/(?P<pk>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})'
--- proyectoFinal/proyectoFinal/settings.py
+++ proyectoFinal/proyectoFinal/settings.py
@@ -125,6 +125,12 @@ MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
